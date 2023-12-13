b0c9701afc6cbe780194b1fab7a12593a8a508e9
mohammad amin alavian <m.amin.alavian@gmail.com>
2023-12-14 00:39:39 +0330

    Rename table001 to T001

--- tests/test_by_results/results/test_results.py
+++ tests/test_by_results/results/test_results.py
@@ -13,7 +13,9 @@ def _eq_tables(first: pd.DataFrame, second: pd.DataFrame, error: float = ERROR):
     return first.sub(second).abs().div(first).mul(100).ge(error).sum().sum() == 0
 
 
-def test_1401_table001():
-    original = pd.read_csv(CSV_DIR.joinpath("1401_001.csv"), header=[0, 1], index_col=0)
-    created = lfsir.load_knowledge("sci_results.table001", 1401)
+def test_1401_T001():
+    original = pd.read_csv(
+        CSV_DIR.joinpath("1401_T001.csv"), header=[0, 1], index_col=0
+    )
+    created = lfsir.load_knowledge("sci_results.T001", 1401)
     assert _eq_tables(original, created)
